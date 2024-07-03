88c08aaf0daa965ec28134764c9ab3f6ed9485fa
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2024-07-03 18:52:26 +0530

    Add files via upload

---
+++ examples/Demo_MotorMode_Single/Demo_MotorMode_Single.ino
@@ -0,0 +1,33 @@
+/*
+* Motor Mode Example
+*/
+
+#include <SBServo.h>
+
+SBServo sb; //create instance
+
+// GPIO 18 - S_RXD, GPIO 17 - S_TXD, as default.
+#define S_RXD 18
+#define S_TXD 17
+
+const int ID = 0x02; // Change Motor as per your Serial Servo Motor 
+
+void setup()
+{
+  Serial1.begin(115200, SERIAL_8N1, S_RXD, S_TXD);
+  sb.pSerial = &Serial1; 
+  delay(1000);
+  sb.PWMMode(ID);  // define ID of motor to control 
+}
+
+void loop()
+{
+  sb.WritePWM(ID, 1000);  // ID, speed 
+  delay(2000);
+  sb.WritePWM(ID, 0);   // stop motor
+  delay(2000);
+  sb.WritePWM(ID, -1000);  // reverse direction
+  delay(2000);
+  sb.WritePWM(ID,0);
+  delay(2000);
+}
